2ab554137d1722a7d08fa3bb1ccbde1d9be4d53e
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2025-07-13 19:33:43 -0700

    pagina historia

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,3 @@
-/*TOGGLE MENU BOTON RESPONSIVE*/
-function mostrarMenu() {
-  const nav = document.querySelector(".header__nav");
-  const toggle = document.getElementById("navToggle");
-
-  nav.classList.toggle("header__nav--visible");
-  toggle.classList.toggle("nav__toggle--abierto");
-
-  // Alternar el valor de aria-expanded
-  const expanded = toggle.getAttribute("aria-expanded") === "true";
-  toggle.setAttribute("aria-expanded", String(!expanded));
-}
 
 /*CERRAR Y MOSTRAR INFO DESPEGABLE*/
 function mostrarInfoContenedor(elemento, id) {
